8645e144b35d64c02d98f25da53e6874bdac28c7
dante065 <leonsiliao@gmail.com>
2022-10-13 14:55:49 +0800

    Update PayPalConfig.java

--- app/src/main/java/com/tian/uber/PayPalConfig.java
+++ app/src/main/java/com/tian/uber/PayPalConfig.java
@@ -1,7 +1,1 @@
-package com.tian.uber;
 
-
-public class PayPalConfig {
-    public static final String PAYPAL_CLIENT_ID = "Abcaf2Ym8nmDk9wu9RC_74kUbxpRlvD5jreJrMS9H95MnXaPM8UY2RRQJ4QxOxitWO9s46FmvFEoipFh";
-    public static final String PAYPAL_PAYOUT_URL = "https://us-central1-uber-32147.cloudfunctions.net/payout";
-}
