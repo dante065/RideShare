ae35fa8a99bb602bdc9a5dd756d43a603df87ba4
dante065 <leonsiliao@gmail.com>
2022-10-13 14:57:46 +0800

    Update ExampleUnitTest.java

--- app/src/test/java/com/tian/uber/ExampleUnitTest.java
+++ app/src/test/java/com/tian/uber/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.simcoder1.uber;
+
 
 import org.junit.Test;
 
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
+ *  package com.simcoder1.uber;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
